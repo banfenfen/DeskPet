7feb3975d8077af8047a359934474575f4acc259
MyShirt <20373202@buaa.edu.cn>
2022-06-11 22:31:28 +0800

    update MoneyState

--- src/org/taibai/hellohei/state/MoneyState.java
+++ src/org/taibai/hellohei/state/MoneyState.java
@@ -4,5 +4,22 @@ public class MoneyState {
 
     private int TotalMoney = 60;
 
+    /**
+     * 余额降低
+     */
+    public void reduce(int cost) {
+        TotalMoney = TotalMoney - cost;
+    }
+
+    /**
+     * 余额增加
+     *
+     * @param num 增加的量
+     */
+    public void increase(int num) {
+        TotalMoney = TotalMoney + num;
+        System.out.printf("[MoneyState::increase(%d)]-当前余额=%d\n", num, TotalMoney);
+    }
+
 
 }
